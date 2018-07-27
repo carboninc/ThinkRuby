107c8dab6b72b5bfd5838d9fd9071f0aedda97c9
Alastar Corr <house.labs@gmail.com>
2018-07-27 11:36:54 +0600

    Correct Homework

--- 3/railway_manager.rb
+++ 3/railway_manager.rb
@@ -2,35 +2,24 @@
 class Station
   def initialize(name)
     @name = name
-    @trains_on_station = []
+    @trains = []
   end
 
-  def train_arrival(type)
-    if %w[пассажирский грузовой].include?(type)
-      @trains_on_station.push(type)
-    else
-      puts 'Поезд может быть только грузовым или пассажирским'
-    end
+  def train_arrival(train)
+    @trains.push(train)
   end
 
-  def trains_on_station
-    puts "Поездов на странции в данный момент: #{@trains_on_station.length}"
+  def trains
+    @trains.length
   end
 
-  def type_trains_on_station
-    cargo = @trains_on_station.select { |train| train == 'грузовой' }
-    passenger = @trains_on_station.select { |train| train == 'пассажирский' }
-
-    puts "Кол-во грузовых поездов: #{cargo.length}"
-    puts "Кол-во пассажирских поездов: #{passenger.length}"
+  def type_of_trains(type)
+    type_of_trains = @trains.select { |train| train.type == type }
+    type_of_trains.length
   end
 
-  def train_departure(type)
-    if %w[пассажирский грузовой].include?(type) && !@trains_on_station.empty?
-      @trains_on_station.delete_at(@trains_on_station.find_index(type))
-    else
-      puts 'Ошибка при указании типа поезда или такого поезда нет на станции'
-    end
+  def train_departure(train)
+    @trains.include?(train) ? @trains.delete(train) : nil
   end
 end
 
@@ -46,80 +35,61 @@ class Route
   end
 
   def delete_station(name_station)
-    if [@stations.first, @stations.last].include?(name_station)
-      puts 'Эту станцию нельзя удалить'
-    else
-      @stations.delete_at(@stations.find_index(name_station))
-    end
-  end
-
-  def show_stations
-    puts 'Список станций данного маршрута:'
-    @stations.each do |station|
-      puts station
-    end
+    [@stations.first, @stations.last].include?(name_station) ? nil : @stations.delete_at(@stations.find_index(name_station))
   end
 end
 
 # Train Class
 class Train
+  attr_reader :type, :wagons
   attr_accessor :speed
   def initialize(number, type, count)
-    @train = { train: { number: number, type: type, wagons: count } }
+    @number = number
+    @type = type
+    @wagons = count
     @speed = 0
   end
 
-  def wagons_count
-    puts "Кол-во вагонов в поезде: #{@train[:train][:wagons]}"
+  def add_wagon
+    @speed.zero? ? @wagons += 1 : nil
   end
 
-  def add_or_delete_wagons(type)
-    if @speed.zero?
-      if type == 'add' && @train[:train][:wagons] > 0
-        @train[:train][:wagons] += 1
-      elsif type == 'delete' && @train[:train][:wagons] > 0
-        @train[:train][:wagons] -= 1
-      else
-        'У поезда нет вагонов'
-      end
-    else
-      puts 'Поезд сначала должен остановиться'
-    end
+  def delete_wagon
+    @speed.zero? && @wagons > 0 ? @wagons -= 1 : nil
   end
 
   def route(route)
     @route = route
     @current_station = 0
-    puts "Поезд начинает свой маршрут со станции: #{@route.stations.first}"
   end
 
   def move_forward
     if @current_station >= @route.stations.length - 1
-      puts "#{@route.stations.last} - конечная станция"
+      @route.stations.last
     else
-      puts "Станция: #{@route.stations[@current_station + 1]}"
+      @route.stations[@current_station + 1]
       @current_station += 1
     end
   end
 
   def move_backward
     if @current_station.zero?
-      puts "#{@route.stations.first} - начальная станция"
+      @route.stations.first
     else
-      puts "Станция: #{@route.stations[@current_station - 1]}"
+      @route.stations[@current_station - 1]
       @current_station -= 1
     end
   end
 
   def current_station
     if @current_station.zero?
-      puts "Начальная станция, следующая станция #{@route.stations[1]}"
+      @route.stations[1]
     elsif @current_station == @route.stations.length - 1
-      puts "Конечная станция, предыдущая станция #{@route.stations[-2]}"
+      @route.stations[-2]
     else
-      puts "Вы находитесь на станции: #{@route.stations[@current_station]}"
-      puts "Предыдущая станция: #{@route.stations[@current_station - 1]}"
-      puts "Следующая станция: #{@route.stations[@current_station + 1]}"
+      @route.stations[@current_station]
+      @route.stations[@current_station - 1]
+      @route.stations[@current_station + 1]
     end
   end
 end
